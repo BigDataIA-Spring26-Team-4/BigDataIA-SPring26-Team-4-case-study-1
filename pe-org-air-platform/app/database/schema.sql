66658a50718e23bc5dbbb4076f67c2a7856e546a
smmcavoy <mcavoyseamus@gmail.com>
2026-01-30 21:32:14 -0500

    api skeletons

---
+++ pe-org-air-platform/app/database/schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE industry (
+    id VARCHAR(36) PRIMARY KEY DEFAULT UUID_STRING(),
+    name VARCHAR(255) NOT NULL,
+    h_r_base FLOAT NOT NULL,
+    sector: VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE company (
+    id VARCHAR(36) PRIMARY KEY DEFAULT UUID_STRING(),
+    name VARCHAR(255) NOT NULL,
+    ticker VARCHAR(10) NOT NULL,
+    industry_id VARCHAR(36) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (industry_id) REFERENCES industry(id)
+);
+
+CREATE TABLE assessment (
+    id VARCHAR(36) PRIMARY KEY DEFAULT UUID_STRING(),
+    company_id VARCHAR(36) NOT NULL,
+    
+
+    FOREIGN KEY (company_id) REFERENCES company(id)
+);
--- pe-org-air-platform/app/main.py
+++ pe-org-air-platform/app/main.py
@@ -2,6 +2,54 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/health")
 def read_root():
-    return {"Hello": "World"}
+    return "NOT_IMPLEMENTED"
+
+@app.post("/api/v1/companies")
+def create_company():
+    return "NOT_IMPLEMENTED"
+
+@app.get("/api/v1/companies")
+def list_companies():
+    return "NOT_IMPLEMENTED"
+
+@app.get("/api/v1/companies/{company_id}")
+def get_company(company_id: str):
+    return "NOT_IMPLEMENTED"
+
+@app.put("/api/v1/companies/{company_id}")
+def update_company(company_id: str):
+    return "NOT_IMPLEMENTED"
+
+@app.delete("/api/v1/companies/{company_id}")
+def delete_company(company_id: str):
+    return "NOT_IMPLEMENTED"
+
+@app.post("/api/v1/assessments")
+def create_assessment():
+    return "NOT_IMPLEMENTED"
+
+@app.get("/api/v1/assessments")
+def list_assessments():
+    return "NOT_IMPLEMENTED"
+
+@app.get("/api/v1/assessments/{assessment_id}")
+def get_assessment(assessment_id: str):
+    return "NOT_IMPLEMENTED"
+
+@app.patch("/api/v1/assessments/{assessment_id}")
+def update_assessment(assessment_id: str):
+    return "NOT_IMPLEMENTED"
+
+@app.post("/api/v1/assessments/{assessment_id}/scores")
+def add_scores(assessment_id: str):
+    return "NOT_IMPLEMENTED"
+
+@app.get("/api/v1/assessments/{assessment_id}/scores")
+def get_scores(assessment_id: str):
+    return "NOT_IMPLEMENTED"
+
+@app.put("/api/v1/assessments/{assessment_id}/scores")
+def update_scores(assessment_id: str):
+    return "NOT_IMPLEMENTED"
